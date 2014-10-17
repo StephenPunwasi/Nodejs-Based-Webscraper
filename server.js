f22d8a172799c15b9ddfb311bb1ea7bd206e2591
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-16 21:24:24 -0400

    Scrape Movie Example

--- server.js
+++ server.js
@@ -7,7 +7,26 @@ var port = process.env.PORT || 8080;
 
 
 app.get('/scrape', function(req, res){
-  //Scrapies go here
+  url = 'http://www.imdb.com/title/tt1229340/'
+  request(url, function(error, response, html){
+      if(!error){
+        var $ = cheerio.load(html);
+        var title, release, rating;
+        var json = {title :"", release:"",rating:""};
+        $(".header").filter(function(){
+          var data = $(this);
+          title = data.children().first().text();
+          json.title = title;
+          json.release = release;
+        })
+        $('.star-box-giga-star').filter(function(){
+          var data = $(this);
+          rating = data.text();
+          json.rating = rating;
+        })
+      }
+      
+  });
 });
 
 app.listen(port);
